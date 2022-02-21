16acd41c0013da5bb140eab5a210eeba7d552c2e
AntonChb <selkup-89@mail.ru>
2022-02-21 17:33:18 +0300

    add lod4j2.xml

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,18 +9,49 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	private static Logger logger;
+
 	@Autowired
 	DemoApplicationService demoApplicationService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
 
-	public int sum ( int one, int two){
+		logger = LogManager.getRootLogger();
+		int x = 5;
+		if (x > 7) {
+			System.out.println("Правильно");
+		} else {
+			logger.info("В параметре Х-написана хуйня");
+			System.out.println("Ошибка");
+		}
+
+
+//	public Integer y() {
+//		return DemoApplication.integer();
+//	}
+//
+//
+//	public static Integer integer() {
+//		logger = LogManager.getRootLogger();
+//		int x = 5;
+//		if (x > 7) {
+//			System.out.println("Правильно");
+//		} else {
+//			logger.info("В параметре Х-написана хуйня");
+//			System.out.println("Ошибка");
+//		}
+//		return x;
+//	}
+
+	}
+	public int sum(int one, int two) {
 		return one + two;
 	}
-	public int result ( int one, int two){
+
+	public int result(int one, int two) {
 		DemoApplicationService q = new DemoApplicationService();
 		return q.multiply(one, two);
 	}
 }
+
